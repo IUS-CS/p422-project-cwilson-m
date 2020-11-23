17ccc62f62137e2eb93140c8890e996fc0032a11
cwilson-m <70290978+cwilson-m@users.noreply.github.com>
2020-11-23 16:02:13 -0500

    first commit

---
+++ app/view1/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Auto insurance',
+        price: 799,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Home insurance',
+        price: 699,
+        description: 'A great phone with one of the best cameras'
+    },
+    {
+        name: 'Boat insurance',
+        price: 299,
+        description: ''
+    }
+];
